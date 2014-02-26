7c5f4709268972339659db111f1cf710d16560fc
JorgeBailo <github@jorgebailo.es>
2014-02-26 16:31:38 +0100

    Validations Producto

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,2 +1,7 @@
 class Producto < ActiveRecord::Base
+  
+  validates_presence_of :name, :description, :manufacturer, :units
+  validates_length_of :name, :minimum => 3 , :maximum => 255
+  validates_length_of :manufacturer, :minimum => 3 , :maximum => 255
+  validates_numericality_of :units, :greater_than => 0
 end
